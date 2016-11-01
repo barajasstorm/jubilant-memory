5a5b215d2a131e5fc5396f39c142baf0315f26ed
barajasstorm <jbarajas@servsuite.net>
2016-10-31 23:09:04 -0600

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Juan Barajas on 10/31/16.
+//  Copyright © 2016 Juan Barajas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
